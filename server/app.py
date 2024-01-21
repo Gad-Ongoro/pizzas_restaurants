e6dd9383f180da4901bd4b445539ca12c2744486
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-21 20:49:23 +0300

    Get all restaurants and by ID

--- server/app.py
+++ server/app.py
@@ -1,13 +1,66 @@
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
-from models import db
+from flask_cors import CORS
+from flask_restful import Api, Resource
+from models import db, Restaurants, Pizzas, RestaurantPizza
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+CORS(app)
+api = Api(app)
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
+@app.route('/')
+def home():
+    return("""
+           <div style="display: flex; justify-content: center; align-items: center;">
+                <h1 style="color:green;">PIZZAS & RESTAURANTS</h1>
+           </div>
+           """         
+        )
+
+
+class Rests(Resource):
+    def get(self):
+        rests = [restaurant.to_dict() for restaurant in Restaurants.query.all()]
+        response = make_response(jsonify(rests))        
+        return response
+
+api.add_resource(Rests, '/restaurants', endpoint='restaurants')
+
+class Rest_by_id(Resource):
+    def get(self, id):
+        rest = Restaurants.query.filter_by(rest_id = id).first()
+        
+        response = make_response(jsonify(rest.to_dict()))
+        
+        return response
+    
+api.add_resource(Rest_by_id, '/restaurants/<int:id>')
+    
+
+
+# @app.route('/restaurants/<int:id>')
+# def get_restaurant(id):
+#     pass
+
+# @app.route('/restaurants/<int:id>')
+# def del_restaurant(id):
+#     pass
+
+# @app.route("/pizzas")
+# def get_pizzas():
+#     pass
+
+# @app.route('/restaurant_pizzas')
+# def post_rest_pizzas():
+#     pass
 
 if __name__ == "__main__":
     app.run(port = 5500, debug = True)
